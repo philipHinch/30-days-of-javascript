c76ee61e76a06ffee28d857b0f461265e3bf2dcb
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-08-22 17:01:29 +0300

    commit

---
+++ Day 3/app.js
@@ -0,0 +1,66 @@
+// EXERCISES LEVEL 2
+
+//1 
+
+// let b = prompt('Enter the base value');
+
+// let h = prompt('Enter the height value');
+
+// let area = (b * h) / 2;
+
+// console.log(area);
+
+//2 
+
+// let a = parseInt(prompt('Enter the lenght of a'));
+
+// let b = parseInt(prompt('Enter the lenght of b'));
+
+// let c = parseInt(prompt('Enter the lenght of c'));
+
+// let perimiter = a + b + c;
+
+// console.log(perimiter);
+
+//3 
+
+// let length = parseInt(prompt('Enter rectangle length'));
+
+// let width = parseInt(prompt('Enter rectangle width'));
+
+// let area = length * width;
+
+// let perimeter = (length + width) * 2;
+
+// console.log(area, perimeter);
+
+//4 
+
+// let r = parseInt(prompt('Enter the radius'));
+
+// let circleArea = Math.PI * Math.pow(r, 2);
+
+// let circleCircumference = 2 * Math.PI * r;
+
+// console.log(circleArea, circleCircumference);
+
+//5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
