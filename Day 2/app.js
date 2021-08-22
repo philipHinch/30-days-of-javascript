8148da5241ab846e7d5c426edcbbdbd65875ca54
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-08-22 16:01:33 +0300

    commit

---
+++ Day 2/app.js
@@ -0,0 +1,77 @@
+//LEVEL 2 EXCERCISES
+
+//1
+
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.");
+
+//2
+
+console.log(`
+"Love is not patronizing and charity isn't about pity, it is about love.Charity and love are the same-- with charity you give love, so don't just give money but reach out your hand instead."
+    `);
+
+//3
+
+let num = '10';
+
+let newNum = parseInt(num);
+
+console.log(typeof newNum);
+
+//4
+
+let num2 = parseFloat('9.8');
+
+console.log(Math.ceil(num2));
+
+//5
+
+let language1 = 'python';
+let language2 = 'jargon';
+
+console.log(language1.includes('on'), language2.includes('on'));
+
+//6
+
+let phrase = 'I hope this course is not full of jargon';
+
+console.log(phrase.includes('jargon'));
+
+//7
+
+let randomNum = Math.floor(Math.random() * 101);
+
+console.log(randomNum);
+
+//8
+
+let randomNum2 = 50 + Math.floor(Math.random() * 51);
+
+console.log(randomNum2);
+
+//9
+
+let randomNum3 = Math.floor(Math.random() * 256);
+
+console.log(randomNum3);
+
+//10
+
+let string = 'JavaScript';
+
+console.log(string.charAt(Math.floor(Math.random() * string.length)));
+
+//11
+
+//12 
+
+let phrase2 = 'You cannot end a sentence with because because because is a conjunction';
+
+console.log(phrase2.substr(31, 24));
+
+
+
+
+
+
+
