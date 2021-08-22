d2f2189f1d730da20202cbabda2c3ace3d119bcd
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-08-22 11:14:10 +0300

    commit

---
+++ Day 1/datatypes.js
@@ -0,0 +1,6 @@
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof name);
+console.log(typeof likesJavascript);
+console.log(typeof motorbike);
+console.log(typeof boat);
---
+++ Day 1/variable.js
@@ -0,0 +1,39 @@
+// declare variables and assign string, boolean, undefined and null data types
+
+const name = 'Philip';
+const likesJavascript = true;
+const motorbike = undefined;
+const boat = null;
+
+// Declare four variables without assigning values
+
+let surname;
+let number;
+let favouriteDish;
+let color;
+
+// Declare four variables with assigned values
+
+let team = 'Juventus';
+let livesInItaly = false;
+let scooter = undefined;
+let shoeSize = 46;
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let myFirstName = 'Philip';
+let myLastName = 'Hinchsliff';
+let myMaritalStatus = 'Cohabitating';
+let myCountry = 'Finland';
+let myHeight = 186;
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstName = 'Phil', lastName = 'Hinch', maritalStatus = 'single', country = 'Italy', age = '31';
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 30;
+let yourAge = 30;
+
+console.log(myAge, yourAge);
