e4fc8e1d2f6c98b7a46833e5b0ec31c9e827a651
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-08-24 16:31:18 +0300

    commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// EXERCISE LEVEL 2
+
+//1 Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+
+//2 First remove all the punctuations and change the string to array and count the number of words in the array
+
+// let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.';
+
+// let newText = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").split(' ');
+
+// console.log(newText.length);
+
+//3 In the following shopping cart add, remove, edit items
+
+// const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+
+// shoppingCart.push('apples', 'chicken', 'eggs');
+
+// shoppingCart.unshift('whiskey');
+
+// console.log(shoppingCart);
+
+//4 In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+
+// if (countries.includes('Ethiopia')) {
+//     console.log('ETHIOPIA');
+// } else {
+//     countries.push('Ethiopia');
+// }
+
+// console.log(countries);
+
+//5 In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+
+// webTechs.includes('Sass') ? console.log('Sass is a CSS preprocess') : webTechs.push('Sass');
+
+// console.log(webTechs);
+
+// webTechs.includes('Sass') ? console.log('Sass is a CSS preprocess') : webTechs.push('Sass');
+
+//6 Concatenate the following two variables and store it in a fullStack variable.
+
+// const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+// const backEnd = ['Node', 'Express', 'MongoDB'];
+// const fullStack = frontEnd.concat(backEnd);
+
+// console.log(fullStack);
+
+
+
