91802a3b2603f0f8d07862db2a1a71dfa1024d11
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-08-25 17:07:49 +0300

    commit

---
+++ Day 6/app.js
@@ -0,0 +1,24 @@
+// EXERCISE LEVEL 1
+
+//1 Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+// for (let i = 0; i < 11; i++) {
+//     console.log(i);
+// };
+
+// let i = 0;
+
+// while (i < 11) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 0
+// do {
+//     console.log(i)
+//     i++
+// } while (i <= 10)
+
+
+
+
